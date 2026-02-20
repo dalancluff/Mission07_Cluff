a43df3c2074d5dd25e3f4dc6f7eb8f17fc9499d8
Dalan Cluff <dalan.cluff@gmail.com>
2026-02-20 00:13:14 -0700

    Changed names to Mission07

--- Mission06_Cluff/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Mission06_Cluff.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
